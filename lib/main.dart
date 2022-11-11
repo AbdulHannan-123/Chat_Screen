8392933804078aee6d6d791b6e27763a9c73ff42
AbdulHannan-123 <ah6475774@gmail.com>
2022-11-12 00:16:25 +0500

    creating user and save its data in DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:chat_screen_for/login_account_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-Future main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
-
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    // ignore: prefer_const_constructors
+    options: FirebaseOptions(
+        apiKey: "AIzaSyCTrZlFbbifqIsdx-Q1Rh4LMDJP9W0sfW0",
+        appId: "",
+        messagingSenderId: "",
+        projectId: "one2onechat-6151f"),
+  );
   runApp(MyApp());
 }
 
@@ -13,8 +20,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
