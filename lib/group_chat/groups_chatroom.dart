c2297131f07bce7878db14d7fef14de60b3945b7
AbdulHannan-123 <ah6475774@gmail.com>
2022-12-08 21:20:00 +0500

    mini mizes the code

--- lib/group_chat/groups_chatroom.dart
+++ lib/group_chat/groups_chatroom.dart
@@ -26,32 +26,6 @@ class GroupChatRoom extends StatelessWidget {
     }
   }
 
-  // List<Map<String, dynamic>> dynamicChatList = [
-  //   {
-  //     "message": "User1 Created the group",
-  //     "type": "notify",
-  //   },
-  //   {
-  //     "message": "Hello this is user1",
-  //     "send_by": "User1",
-  //     "type": "text",
-  //   },
-  //   {
-  //     "message": "Hello this is user 2",
-  //     "send_by": "User4",
-  //     "type": "text",
-  //   },
-  //   {
-  //     "message": "Hello this is user 4",
-  //     "send_by": "User6",
-  //     "type": "text",
-  //   },
-  //   {
-  //     "message": "Hello this is user 6",
-  //     "send_by": "User8",
-  //     "type": "text",
-  //   },
-  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -195,18 +169,8 @@ class GroupChatRoom extends StatelessWidget {
                       right: 140, top: 3, bottom: 3, left: 10)
                   : const EdgeInsets.only(
                       left: 140, top: 3, bottom: 3, right: 10),
-              // decoration: BoxDecoration(
-              //     borderRadius: BorderRadius.circular(15), color: Colors.blue),
               height: size.height / 2,
               child: Image.network(chatMap['message']),
-              //  Text(
-              //   chatMap['message'],
-              //   style: const TextStyle(
-              //     color: Colors.white,
-              //     fontSize: 16,
-              //     fontWeight: FontWeight.w500,
-              //   ),
-              // ),
             ),
           );
         } else if (chatMap['type'] == 'notify') {
@@ -214,21 +178,11 @@ class GroupChatRoom extends StatelessWidget {
             color: Colors.red,
             width: size.width,
             alignment: Alignment.center,
-            // padding: EdgeInsets.symmetric(
-            //   horizontal: size.width / 100,
-            //   vertical: size.height / 400,
-            // ),
             child: Center(
               child: Container(
-                // alignment:Alignment.center,
 
                 padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
                 margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
-                // margin: chatMap['send_by'] != currentUserName
-                //     ? const EdgeInsets.only(
-                //         right: 140, top: 3, bottom: 3, left: 10)
-                //     : const EdgeInsets.only(
-                //         left: 140, top: 3, bottom: 3, right: 10),
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
                     color: Colors.black38),
